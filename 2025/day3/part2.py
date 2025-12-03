04155cf63a9ac6f2244ea92490930b0e0c48bfaa
Kurz <emanuel.kurz@it.htl-hl.ac.at>
2025-12-03 16:31:24 +0100

    2025 day 3 p2 init

---
+++ 2025/day3/part2.py
@@ -0,0 +1,11 @@
+with open("data.txt") as f:
+    _input = f.read().splitlines()
+
+def find_max(pack):
+    numbers = [0] * 12
+    for n in pack:
+        break
+    return 0
+
+
+print(sum(find_max(pack) for pack in _input))
